1f2f4c14ac83230a5b82f727235d458989fb8bc3
Liqun Liu <liqunl@ca.ibm.com>
2019-12-17 17:52:15 -0500

    Add ThreadLocalBench
    Signed-off-by: Liqun Liu <liqunl@ca.ibm.com>

---
+++ net/adoptopenjdk/bumblebench/humble/ThreadLocalBench.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.humble;
+
+import net.adoptopenjdk.bumblebench.core.HumbleBench;
+
+public final class ThreadLocalBench extends HumbleBench {
+
+	public ThreadLocalHelper helper;
+	public static String TEXT = option(
+		"text",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit");
+
+	public ThreadLocalBench() { super(Workload.class); }
+
+
+	@Override
+	protected final void setup(int numIterations) {
+		// Test on one thread local
+		helper = new ThreadLocalHelper();
+	}
+
+	public static class Workload extends AbstractWorkload {
+		@Override
+		public void doBatch(HumbleBench bench0, int numIterations) {
+			ThreadLocalBench bench = (ThreadLocalBench)bench0;
+
+			for (int i = 0; i < numIterations; i++) {
+				bench.helper.getThreadId();
+			}
+		}
+	}
+
+}
